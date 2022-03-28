634a1e837a95a4d78008c0e470c53eb1af54e2d2
Giromo <giromi@cau.ac.kr>
2022-03-28 21:53:53 +0900

    go

---
+++ bonus/analysis_pct_bonus 3.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   analysis_pct_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hchang <hchang@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/24 14:51:32 by jaesjeon          #+#    #+#             */
+/*   Updated: 2022/03/13 14:52:29 by minsuki2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf_bonus.h"
+
+static void	solve_conflict(t_gather *fwp)
+{
+	if ((fwp->bits & FG_ZERO) && (fwp->bits & FG_MINUS))
+		fwp->bits &= ~FG_ZERO;
+	if ((fwp->bits & FG_PLUS) && (fwp->bits & FG_SPACE))
+		fwp->bits &= ~FG_SPACE;
+	if ((fwp->bits & FG_ZERO) && (fwp->bits & PC_EXIST))
+		fwp->bits &= ~FG_ZERO;
+	if (fwp->bits & CV_I)
+		fwp->bits |= CV_D;
+	if (fwp->bits & (CV_C | CV_PCT))
+		fwp->bits &= ~(FG_PLUS | FG_SPACE | FG_POUND | PC_EXIST);
+	else if (fwp->bits & CV_S)
+		fwp->bits &= ~(FG_ZERO | FG_PLUS | FG_SPACE | FG_POUND);
+	else if (fwp->bits & CV_P)
+	{
+		fwp->bits &= ~(FG_PLUS | FG_SPACE);
+		fwp->bits |= FG_POUND;
+	}
+	else if (fwp->bits & CV_D)
+		fwp->bits &= ~(FG_POUND);
+	else if (fwp->bits & CV_U)
+		fwp->bits &= ~(FG_PLUS | FG_SPACE | FG_POUND);
+	else if (fwp->bits & (CV_SX | CV_LX))
+		fwp->bits &= ~(FG_PLUS | FG_SPACE);
+}
+
+
+static void	find_width_precision(const char **cur, t_gather *fwp)
+{
+	if (ft_isdigit(**cur))
+		fwp->bits |= WD_EXIST;
+	while (**cur && ft_isdigit(**cur))
+	{
+		if ((fwp->width > INT_MAX / 10) || ((fwp->width == INT_MAX / 10)
+				&& ((**cur - '0') >= INT_MAX % 10)))
+		{
+			fwp->bits |= FAIL_INT;
+			return ;
+		}
+		fwp->width = fwp->width * 10 + (**cur - '0');
+		(*cur)++;
+	}
+	if (**cur == '.' && (*cur)++)
+		fwp->bits |= PC_EXIST;
+	while (**cur && ft_isdigit(**cur))
+	{
+		fwp->precision = fwp->precision * 10 + (**cur - '0');
+		(*cur)++;
+	}
+}
+
+
+static void	find_conversion(const char **cur, int *bits)
+{
+	int	idx;
+
+	idx = ft_strchr_idx(CV_SET, **cur, RTN_ERROR);
+	if (**cur && idx != ERROR)
+	{
+		*bits |= 1 << (CV_BIT_BEGIN  - idx);
+		(*cur)++;
+	}
+	else
+		*bits |= FAIL_CONV;
+}
+
+static void	find_flags(const char **cur, int *bits)
+{
+	int	idx;
+
+	while (**cur)
+	{
+		idx = ft_strchr_idx(FG_SET, **cur, RTN_ERROR);
+		if (idx == ERROR)
+			break ;
+		*bits |= 1 << (FG_BIT_BEGIN - idx);
+		(*cur)++;
+	}
+}
+
+void	analysis_pct(const char **cur, t_gather *fwp)
+{
+	(*cur)++;
+	find_flags(cur, &fwp->bits);
+	find_width_precision(cur, fwp);
+	find_conversion(cur, &fwp->bits);
+	if (!(fwp->bits & (FAIL_INT| FAIL_CONV)))
+		solve_conflict(fwp);
+}
---
+++ bonus/ft_printf_utils_bonus 3.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils_bonus.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minsuki2 <minsuki2@student.42seoul.kr      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/07 01:39:46 by minsuki2          #+#    #+#             */
+/*   Updated: 2022/03/13 16:13:19 by minsuki2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf_bonus.h"
+
+int	salloc_int(char **new, int len, char fill)
+{
+	*new = (char *)malloc((len + 1) * sizeof(char));
+	if (!*new)
+		return (ERROR);
+	(*new)[len] = '\0';
+	ft_memset(*new, fill, len);
+	return (SUCCESS);
+}
+
+int	ft_strchr_idx(const char *s, int c, int switcher)
+{
+	int	idx;
+
+	idx = 0;
+	while (s[idx])
+		if (s[idx++] == (char)c)
+			return (idx - 1);
+	if (switcher)
+		return (idx);
+	return (ERROR);
+}
+
+
+int	check_len_max(int *cnt, int check)
+{
+	if (FINAL_LEN - *cnt < check)
+		return (ERROR);
+	*cnt += check;
+	return (SUCCESS);
+}
+
+int	full_len_check(t_gather *fwp, int *len)
+{
+	int	full_len;
+
+	if (fwp->bits & PC_EXIST && fwp->precision > *len)
+		*len = fwp->precision;
+	full_len = fwp->width;
+	if (*len > fwp->width)
+		full_len = *len;
+	if (isfg_incr(fwp) && (full_len == *len || full_len == *len + 1))
+		full_len += isfg_incr(fwp) - (full_len - *len);
+	if (full_len < 0 || full_len == INT_MAX)
+		return (ERROR);
+	fwp->full_len = full_len;
+	return (SUCCESS);
+}
+
+int isfg_incr(t_gather *fwp)
+{
+	if (fwp->bits & FG_POUND)
+		return (2);
+	return ((fwp->bits & (FG_PLUS | FG_SPACE)) != 0);
+}
